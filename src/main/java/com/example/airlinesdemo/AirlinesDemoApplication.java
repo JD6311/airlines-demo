88bb1c86f4986c6376d4a129ddaff06c26547a22
ue <jaydeeppatil63110@gmail.com>
2023-05-12 07:20:27 +0530

    codeRefactor

---
+++ src/main/java/com/example/airlinesdemo/AirlinesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlinesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesDemoApplication.class, args);
+	}
+
+}
